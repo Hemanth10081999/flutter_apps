ad8c5ef83ea2a79d2e23e4b1bfdcac9c2a8fdf79
teamdriptech <vhemantharajan@gmail.com>
2020-02-07 23:03:23 +0530

    icons and buttons

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Its working'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('hello 123'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
-));
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Its working'),
+        centerTitle: true,
+        backgroundColor: Colors.red[800],
+      ),
+      body: Center(
+          child: IconButton(
+            onPressed: (){
+              print('printer pressed');
+            },
+            icon: Icon(
+                Icons.print,
+                size: 50.0,
+            ),
+          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
