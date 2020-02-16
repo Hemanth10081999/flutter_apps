348beb3940fa30baf9aaf4f305229500fceae665
teamdriptech <vhemantharajan@gmail.com>
2020-02-16 14:59:36 +0530

    Id card app

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -4,31 +4,132 @@ void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
-class Home extends StatelessWidget {
+
+
+
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int age = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[700],
       appBar: AppBar(
-        title: Text('Its working'),
+        title: Text('Your Profile'),
         centerTitle: true,
-        backgroundColor: Colors.red[800],
+        backgroundColor: Colors.grey[900],
+        elevation: 0.0,
       ),
-      body: Center(
-          child: IconButton(
-            onPressed: (){
-              print('printer pressed');
-            },
-            icon: Icon(
-                Icons.print,
-                size: 50.0,
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/123.jpg'),
+                radius: 100.0,
+              ),
             ),
-          ),
+            Divider(
+              height: 40.0,
+              color: Colors.grey[100],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Hemanth',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'Age',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '$age',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[100],
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Email',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+
+              ],
+            ),
+            Text(
+              'vhemantharajan@gmal.com',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.red[600],
+        onPressed: () {
+          setState(() {
+            age+=1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
     );
   }
 }
+
+
+
